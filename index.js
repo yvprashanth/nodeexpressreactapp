4d9c5737ca1ad6e6fd467369f909fba5acab3c11
Prashanth <prashanthyv@gmail.com>
2018-12-02 14:14:56 -0800

    Deploy

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cowsay = require('cowsay')
 const cors = require('cors')
 // Create the server
 const app = express()
+const path = require('path')
+
+
 // Serve our api route /cow that returns a custom talking text cow
 app.get('/api/cow/:say', cors(), async (req, res, next) => {
     try {
@@ -22,6 +25,18 @@ app.get('/api/cow/', cors(), async (req, res, next) => {
         next(err)
     }
 })
+
+
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+
+
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
